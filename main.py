75e00c790331cf14d5b1c815cf7c0d6098779b07
Hamza Ali Zahid <hamza.ali1012@gmail.com>
2026-06-04 12:37:15 +0500

    Phase 2.3

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ load_dotenv()
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GEMINI_MODEL = "gemini-3.5-flash"
+SOURCE_TYPE_WEIGHTS = {"academic" : 2, "government" : 1.9, "organization" : 1.5, "news" : 1.4, "unknown" : 1.0, "blog" : 0.6, "social" : 0.4}
 
 gemini_client = genai.Client(api_key = GEMINI_API_KEY)
 
 st.title("AI Claim Verification Agent")
 claim = st.text_area("Write your claim here:")
 
-def gemini_response(prompt):
+def gemini_response(gemini_prompt):
     response = gemini_client.models.generate_content(model = GEMINI_MODEL, contents = gemini_prompt).text
     return response
 
 def ask_gemini_source_type(domain):
-    gemini_prompt = f"I will give you a domain and your job is to tell me the most likely source this domain refers to. Your response must be one word from these: academic, news, government, organization, social, blog, unknown. Output unknown if you are unsure about the source\nDomain: {domain}"
-    response = gemini_response(gemini_prompt).lower()
+    prompt = f"I will give you a domain and your job is to tell me the most likely source this domain refers to. Your response must be one word from these: academic, news, government, organization, social, blog, unknown. Output unknown if you are unsure about the source\nDomain: {domain}"
+    response = gemini_response(prompt).lower()
     return response
 
 def get_source_type(url):
@@ -44,6 +45,6 @@ if st.button("Verify Claim"):
         tavily_response = tavily_response["results"]
         for response in tavily_response:
             content = response["content"]
-            gemini_prompt = f"I am going to provide you with a claim and evidence. Your job is to see the claim and evidence and decide whether the evidence is supporting the claim, contradicting the claim, or neutral. You can only give one word answer from 3 words: claim, contradicting, neutral\nClaim: {claim}\nEvidence: {content}"
-            response = gemini_response(gemini_prompt).lower()
-            st.write(gemini_response)
+            prompt = f"I am going to provide you with a claim and evidence. Your job is to see the claim and evidence and decide whether the evidence is supporting the claim, contradicting the claim, or neutral. Also give a credibility score ranging from 1 to 10 (both inclusive). \nUse this criteria to score: Does it make factual claims or just mentions opinions, Does it mention data or statistics, Does it refer or cite other sources, Is language neutral or emotional.\nYour response should be in the exact format: label score where label can be supporting, contradicting, or neutral and score is integer number.\nClaim: {claim}\nEvidence: {content}"
+            response = gemini_response(prompt).lower().strip()
+            st.write(response)
