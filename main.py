70492070191c32c9210db7dbe16983525507c3b2
Hamza Ali Zahid <hamza.ali1012@gmail.com>
2026-06-05 14:59:49 +0500

    Phase 2.6

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ from tavily import TavilyClient
 from dotenv import load_dotenv
 import os
 import streamlit as st
-from google import genai
+from openai import OpenAI
 from urllib.parse import urlparse
 
 load_dotenv()
 
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_MODEL = "gemini-3.5-flash"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+INFERENCE_MODEL = "llama-3.3-70b-versatile"
 SOURCE_TYPE_WEIGHTS = {"academic" : 2, "government" : 1.9, "organization" : 1.5, "news" : 1.4, "unknown" : 1.0, "blog" : 0.6, "social" : 0.4}
 LABEL_WEIGHTS = {"supporting" : 1, "neutral" : 0, "contradicting" : -1}
 
-gemini_client = genai.Client(api_key = GEMINI_API_KEY)
+client = OpenAI(api_key = GROQ_API_KEY, base_url = "https://api.groq.com/openai/v1")
 
 st.title("AI Claim Verification Agent")
 claim = st.text_area("Write your claim here:")
 
-def gemini_response(gemini_prompt):
-    response = gemini_client.models.generate_content(model = GEMINI_MODEL, contents = gemini_prompt).text
-    return response
+def llm_response(llm_prompt):
+    response = client.responses.create(input = llm_prompt, model = INFERENCE_MODEL)
+    return response.output_text
 
-def ask_gemini_source_type(domain):
+def ask_llm_source_type(domain):
     prompt = f"I will give you a domain and your job is to tell me the most likely source this domain refers to. Your response must be one word from these: academic, news, government, organization, social, blog, unknown. Output unknown if you are unsure about the source\nDomain: {domain}"
-    response = gemini_response(prompt).lower()
+    response = llm_response(prompt).lower()
     return response
 
 def get_source_type(url):
@@ -35,7 +35,7 @@ def get_source_type(url):
     elif ".gov" in domain:
         return "government"
     else:
-        response = ask_gemini_source_type(domain)
+        response = ask_llm_source_type(domain)
         return response
     
 def calculate_heuristic_score(predictions):
@@ -50,17 +50,17 @@ def calculate_heuristic_score(predictions):
 
 if st.button("Verify Claim"):
     if claim:
-        gemini_predictions = []
+        llm_predictions = []
         tavily_client = TavilyClient(api_key = TAVILY_API_KEY)
-        tavily_response = tavily_client.search(claim)
+        tavily_response = tavily_client.search(claim, max_results = 3)
         tavily_response = tavily_response["results"]
         for response in tavily_response:
             source_weight = SOURCE_TYPE_WEIGHTS[get_source_type(response["url"])]
             content = response["content"]
             prompt = f"I am going to provide you with a claim and evidence. Your job is to see the claim and evidence and decide whether the evidence is supporting the claim, contradicting the claim, or neutral. Also give a credibility score ranging from 1 to 10 (both inclusive). \nUse this criteria to score: Does it make factual claims or just mentions opinions, Does it mention data or statistics, Does it refer or cite other sources, Is language neutral or emotional.\nYour response should be in the exact format (don't include < and >): <label score> where label can be supporting, contradicting, or neutral and score is integer number.\nClaim: {claim}\nEvidence: {content}"
-            response = gemini_response(prompt).lower().strip().split(' ')
+            response = llm_response(prompt).lower().strip().split(' ')
             source_data = (int(LABEL_WEIGHTS[response[0]]), int(response[1]), source_weight)
-            gemini_predictions.append(source_data)
-        confidence_score = calculate_heuristic_score(gemini_predictions)
+            llm_predictions.append(source_data)
+        confidence_score = calculate_heuristic_score(llm_predictions)
         percentage_confidence = round(confidence_score * 100, 1)
         st.write("Confidence Percentage: ", percentage_confidence)
